5c8cbcbc0a469c4e837619e301bc00e386ae382e
Iaris <iarabecerra24@gmail.com>
2026-04-19 20:05:08 -0300

    lista de turnos

--- gaze-li/src/App.jsx
+++ gaze-li/src/App.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import FormularioTurno from "./components/FormularioTurno";
+import ListaTurnos from "./components/ListaTurnos";
+
 function App() {
+  const [turnos, setTurnos] = useState([]);
+
   return (
     <>
-     <Navbar />
-     <FormularioTurno />
+      <Navbar />
+      <FormularioTurno />
+      <ListaTurnos turnos={turnos} />
     </>
   );
 }
