7565050cbedaae6c7104e591ce1abbf1b007bf7b
Iaris <iarabecerra24@gmail.com>
2026-04-19 19:22:47 -0300

    cree componente navbar

---
+++ gaze-li/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <header>
+      <h1>Gaze</h1>
+
+      <nav>
+        <button>Inicio</button>
+        <button>Turnos</button>
+        <button>Contacto</button>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
